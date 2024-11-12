7934112056e82bf39a8d6f6c6ad5f1fe4e67a886
Elvis Kerebi <elviskerebi@Elviss-MacBook-Pro.local>
2024-11-12 16:28:36 +0200

    first commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,12 @@
+import {ReactNode} from "react";
+import Navbar from "@/app/components/Navbar";
+
+export default function Layout({ children }: Readonly<{ children: React.ReactNode }>) {
+    return (
+        <main className="font-work-sans">
+            <Navbar />
+
+            {children}
+        </main>
+    )
+}
